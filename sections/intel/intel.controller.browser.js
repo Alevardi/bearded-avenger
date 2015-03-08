9ca5a5435f348b4625a311199f3b2817066a029b
Alevardi <amet.alvirde@gmail.com>
2015-03-07 22:41:08 -0600

    :rocket: maps, bike stations and own heatmaps added.

--- sections/intel/intel.controller.browser.js
+++ sections/intel/intel.controller.browser.js
@@ -7,6 +7,12 @@ module.exports = function (app) {
   function intel (scope, cssInjector) {
     cssInjector.add('/css/intel.css');
     scope.lol = 'lol';
+    var map = L.mapbox.map('map', 'chemonky.ld4l0ah8').setView([19.43293,-99.16848], 12),
+      addressPoints = [
+        [19.43293,-99.16848],
+        [19.43159,-99.17151]
+      ],
+      heat = L.heatLayer(addressPoints,{radius: 100}).addTo(map);
 
   }
 
